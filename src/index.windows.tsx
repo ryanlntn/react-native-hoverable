b15f5ca9a7e19b02f9533b89e92444d7ff5dee9a
Ryan Linton <ryanlntn@gmail.com>
2022-07-06 00:43:27 -0600

    fix: update lint rules and fix windows hover

--- src/index.windows.tsx
+++ src/index.windows.tsx
@@ -31,23 +31,16 @@ export const Hoverable: React.FC<HoverableProps> = ({
 export default Hoverable;
 
 export const Pressable: React.FC<PressableProps & MouseEventProps> = ({
+  children,
   onMouseEnter,
   onMouseLeave,
+  onMouseMove,
   style,
   ...props
 }) => {
   const [hovered, setHovered] = useState<boolean>(false);
   return (
     <NativePressable
-      // @ts-ignore
-      onMouseEnter={() => {
-        setHovered(true);
-        onMouseEnter?.();
-      }}
-      onMouseLeave={() => {
-        setHovered(false);
-        onMouseLeave?.();
-      }}
       style={(interactionState) => [
         { position: 'relative' },
         typeof style === 'function'
@@ -55,6 +48,32 @@ export const Pressable: React.FC<PressableProps & MouseEventProps> = ({
           : style,
       ]}
       {...props}
-    />
+    >
+      {(interactionState) => (
+        <>
+          {typeof children === 'function'
+            ? children({ ...interactionState, hovered })
+            : children}
+          <Hoverable
+            onMouseEnter={() => {
+              setHovered(true);
+              onMouseEnter?.();
+            }}
+            onMouseLeave={() => {
+              setHovered(false);
+              onMouseLeave?.();
+            }}
+            onMouseMove={onMouseMove}
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+            }}
+          />
+        </>
+      )}
+    </NativePressable>
   );
 };
